17217ab77cc1af3b8b32d0dc2402dc6ed08e39c1
Karsten Ahnert <karsten.ahnert@gmx.de>
2014-12-19 23:08:30 +0100

    clean up const_step_range

--- examples/const_step_range.cpp
+++ examples/const_step_range.cpp
@@ -14,8 +14,9 @@
  copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#include <boost/numeric/odeint.hpp>
+#include <boost/numeric/odeint/stepper/runge_kutta4.hpp>
 #include <boost/numeric/odeint/range/const_step_range.hpp>
+
 #include <range/v3/view/take.hpp>
 
 #include <iostream>
@@ -26,13 +27,6 @@ const double sigma = 10.0;
 const double R = 28.0;
 const double b = 8.0 / 3.0;
 
-// auto lorenz = []( const auto& x , auto& dxdt , auto t ) 
-// {
-//     dxdt[0] = sigma * ( x[1] - x[0] );
-//     dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
-//     dxdt[2] = -b * x[2] + x[0] * x[1];
-// };
-
 struct lorenz
 {
     template< typename State , typename Deriv , typename Time >
@@ -41,28 +35,6 @@ struct lorenz
         dxdt[0] = sigma * ( x[1] - x[0] );
         dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
         dxdt[2] = -b * x[2] + x[0] * x[1];
-    }
-};
-
-using namespace ranges;
-
-
-// A range that iterates over all the characters in a
-// null-terminated string.
-class c_string_range : public range_facade<c_string_range>
-{
-    friend range_access;
-    char const * sz_;
-    char const & current() const { return *sz_; }
-    bool done() const { return *sz_ == '\0'; }
-    void next() { ++sz_; }
-
-public:
-
-    c_string_range() = default;
-    explicit c_string_range(char const *sz) : sz_(sz)
-    {
-        assert(sz != nullptr);
     }
 };
 
@@ -94,10 +66,6 @@ int main( int argc , char *argv[] )
 
     // ranges::for_each( r , printer {} );
 
-    // c_string_range r2("hello world");
-    // ranges::for_each(r2, [](char ch){
-    //         std::cout << ch << ' ';
-    //     });
     
     return 0;
 }
